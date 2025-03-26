87ca1ccb96fc50e4ce8e85142e0109721a54966a
henry_hung <henryhung942000@gmail.com>
2025-03-26 19:47:07 -0400

    change placeholder text in grid cells

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function createTable() {
         const newRow = tableBody.insertRow();
         for (let j = 1; j <= 15; j++) {
             const cell1 = newRow.insertCell(0);
-            cell1.innerHTML = `garbage ass value`;
+            cell1.innerHTML = `default`;
         }
 
     }
