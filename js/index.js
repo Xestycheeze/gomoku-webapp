f5be3a9a6f32e6c76936ce6bbc16459132d283ba
henry_hung <henryhung942000@gmail.com>
2025-04-04 19:56:08 -0400

    fixed extraction of diag and anti-diag array. add "Draw" as a possible game outcome.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 let boardProgress = [];
-let lengthCondition = 5;
-
+const lengthCondition = 5;
+const boardWidth = 15;
+let unclaimedCellCount = boardWidth * boardWidth;
 let isFirstPlayerTurn = true; // Gomoku is a 2-player game. Use boolean to indicate turns
-let unclaimedCells = 225;
-let lengthCondition = 5;
 
 
 // Function to create 15 rows
 function initTable() {
     const tableBody = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < boardWidth; i++) {
     // Create a new row
         const newRow = tableBody.insertRow();
-        for (let j = 0; j < 15; j++) {
+        for (let j = 0; j < boardWidth; j++) {
             const cell1 = newRow.insertCell(-1);
             cell1.innerHTML = `-1`;
             cell1.setAttribute('data-row', `${i}`)
@@ -24,7 +23,7 @@ function initTable() {
     }
     // it's more elegant to make array from the values captured from HTML, but it's too slow, so
     // instead, just hard code initializing internal JS reference to the HTML table
-    boardProgress = Array.from(Array(15), () => new Array(15).fill(-1));
+    boardProgress = Array.from(Array(boardWidth), () => new Array(boardWidth).fill(-1));
 }
 
 // the elegant way to keep track of the board. Too slow
@@ -34,6 +33,33 @@ function initTable() {
 //     console.log(arr)
 // }
 
+function findCellDiag(row, col){
+    let diags = [];
+    let rowOrigin = (row - col) > 0 ? (row - col) : 0; // assign 0 if the coordinate is in upper echelon or diag line
+    let colOrigin = (row - col) > 0 ? 0 : (col - row); // assign 0 if the coordinate is in lower echelon
+
+    while (rowOrigin < boardProgress.length && (colOrigin < boardProgress[row].length)){
+        diags.push(boardProgress[rowOrigin][colOrigin]);
+        rowOrigin++;
+        colOrigin++;
+    }
+    return diags;
+}
+
+function findCellAntiDiag(row, col){
+    let antiDiags = [];
+    let rowOrigin = ((row + col) >= boardProgress.length) ? ((row + col) - (boardProgress.length - 1)) : 0; // assign 0 if the coordinate is in upper anti-echelon
+    let colOrigin = ((row + col) >= boardProgress.length) ? (boardProgress.length - 1) : (row + col); // assign with right most column if the coordinate is in lower anti-echelon
+
+    while (rowOrigin < boardProgress.length && colOrigin >= 0){
+        antiDiags.push(boardProgress[rowOrigin][colOrigin]);
+        rowOrigin++;
+        colOrigin--;
+    }
+    console.log(antiDiags);
+    return antiDiags;
+}
+
 function spanCount(arr, index){
     // check input validity
     if (arr.length < 1 || index >= arr.length || index < 0){
@@ -43,7 +69,7 @@ function spanCount(arr, index){
     const leftRangeToCheck = (index - (lengthCondition - 1)) < 0 ? 0 : (index - (lengthCondition - 1));
     let rightRange = 0;
     let leftRange = 0;
-    // count the consecutive beads on the left
+    // count the consecutive stones on the left
     for (let i = index; i < rightRangeToCheck; i++){
         if (arr[i] !== -1 && arr[i] === arr[i+1]){
             rightRange++;
@@ -51,7 +77,7 @@ function spanCount(arr, index){
             break;
         }
     }
-    // count the consecutive beads on the left
+    // count the consecutive stones on the left
     for (let i = index; i > leftRangeToCheck; i--){
         if (arr[i] !== -1 && arr[i] === arr[i-1]){
             leftRange++;
@@ -59,89 +85,51 @@ function spanCount(arr, index){
             break;
         }
     }
-    return (rightRange + leftRange + 1)
+    return rightRange + leftRange + 1
 }
 
 function checkVictory(row, col) {
     const rowToBeChecked = boardProgress[row];
     const colToBeChecked = boardProgress.map(r => r[col]);
-    const diagToBeChecked = boardProgress.map((r, index) => r[index]);
-    const antiDiagToBeChecked = boardProgress.map((r, index) => r[boardProgress[row].length - 1 - index]);
+    const diagToBeChecked = findCellDiag(row, col);
+    const antiDiagToBeChecked = findCellAntiDiag(row, col);
     // If the player produces a line of >5 while skipping producing a line of 5, that does not count as victory
     if (spanCount(rowToBeChecked, col) === lengthCondition){
-        return true
+        return true;
     }
     if (spanCount(colToBeChecked, row) === lengthCondition){
-        return true
+        return true;
     }
     if (spanCount(diagToBeChecked, col) === lengthCondition){
-        return true
+        return true;
     }
     if (spanCount(antiDiagToBeChecked, row) === lengthCondition){
-        return true
+        return true;
     }
-    return false
+    return false;
 
 }
 
 function updateBoard(e) {
     if (+e.target.innerText === -1){ // unary plus operator to cast string to int
         console.log(e.target.innerText);
-        let col = e.target.getAttribute('data-col');
-        let row = e.target.getAttribute('data-row');
+        let col = +e.target.getAttribute('data-col'); // unary plus operator to cast string to int
+        let row = +e.target.getAttribute('data-row'); // unary plus operator to cast string to int
         e.target.innerText = isFirstPlayerTurn ? 1 : 0;
         boardProgress[row][col] = isFirstPlayerTurn ? 1 : 0;
+        if (checkVictory(row, col)) {
+            if (isFirstPlayerTurn){
+                console.log("Player 1 won");
+            } else {
+                console.log("Player 2 won");
+            }
+        }
+        unclaimedCellCount--;
+        if (unclaimedCellCount <= 0){
+            console.log("Draw");
+        }
         isFirstPlayerTurn = !isFirstPlayerTurn;
     }
-}
-function spanCount(arr, index){
-    // check input validity
-    if (arr.length < 1 || index >= arr.length || index < 0){
-        return
-    }
-    const rightRangeToCheck = (index + lengthCondition - 1) >= arr.length ? arr.length - 1 : index + lengthCondition - 1;
-    const leftRangeToCheck = (index - (lengthCondition - 1)) < 0 ? 0 : (index - (lengthCondition - 1));
-    let rightRange = 0;
-    let leftRange = 0;
-    // count the consecutive beads on the left
-    for (let i = index; i < rightRangeToCheck; i++){
-        if (arr[i] !== -1 && arr[i] === arr[i+1]){
-            rightRange++;
-        } else {
-            break;
-        }
-    }
-    // count the consecutive beads on the left
-    for (let i = index; i > leftRangeToCheck; i--){
-        if (arr[i] !== -1 && arr[i] === arr[i-1]){
-            leftRange++;
-        } else {
-            break;
-        }
-    }
-    return (rightRange + leftRange + 1)
-}
-
-function checkVictory(row, col) {
-    const rowToBeChecked = boardProgress[row];
-    const colToBeChecked = boardProgress.map(r => r[col]);
-    const diagToBeChecked = boardProgress.map((r, index) => r[index]);
-    const antiDiagToBeChecked = boardProgress.map((r, index) => r[boardProgress[row].length - 1 - index]);
-    // If the player produces a line of >5 while skipping producing a line of 5, that does not count as victory
-    if (spanCount(rowToBeChecked, col) === lengthCondition){
-        return true
-    }
-    if (spanCount(colToBeChecked, row) === lengthCondition){
-        return true
-    }
-    if (spanCount(diagToBeChecked, col) === lengthCondition){
-        return true
-    }
-    if (spanCount(antiDiagToBeChecked, row) === lengthCondition){
-        return true
-    }
-    return false
-
 }
 
 // Call createRows when the page loads
